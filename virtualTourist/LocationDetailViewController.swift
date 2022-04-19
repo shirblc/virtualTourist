66878ec2ca031d12d7e417a260a79a3f2959f220
Shir Bar Lev <shirblc@gmail.com>
2022-04-19 22:14:14 +0300

    Added map setup in the detail view controller

--- virtualTourist/LocationDetailViewController.swift
+++ virtualTourist/LocationDetailViewController.swift
@@ -6,13 +6,35 @@
 //
 
 import UIKit
+import MapKit
+
+let reuseIdentifier = "albumImageCell"
 
 class LocationDetailViewController: UIViewController {
     var dataManager: DataManager!
     var pin: Pin!
-
+    var albums: [PhotoAlbum] = []
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupMap()
     }
+    
+    // MARK: Setup Methods
+    // setupMap
+    // Centres the map on the pin's location and adds the relevant annotation
+    func setupMap() {
+        let locationCoordinate = CLLocationCoordinate2D(latitude: pin.latitude, longitude: pin.longitude)
+        let locationSpan = MKCoordinateSpan(latitudeDelta: CLLocationDegrees(0.5), longitudeDelta: CLLocationDegrees(0.5))
 
+        DispatchQueue.main.async {
+            self.mapView.setRegion(MKCoordinateRegion(center: locationCoordinate, span: locationSpan), animated: true)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate.longitude = self.pin.longitude
+            annotation.coordinate.latitude = self.pin.latitude
+            self.mapView.addAnnotation(annotation)
+        }
+    }
 }
