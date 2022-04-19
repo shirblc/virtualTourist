d188fbfa95ff2e5df03d76633c850d9b2a930e1e
Shir Bar Lev <shirblc@icloud.com>
2022-04-19 13:44:34 +0300

    Set up the initial navigation view controller

--- virtualTourist/MapViewController.swift
+++ virtualTourist/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  virtualTourist
 //
 //  Created by Shir Bar Lev on 19/04/2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
