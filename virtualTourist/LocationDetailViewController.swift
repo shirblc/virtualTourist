3783008df10808ff2a63014dd051d25e2cb31eed
Shir Bar Lev <shirblc@icloud.com>
2022-04-19 20:24:13 +0300

    Added another view controller for the detail view

---
+++ virtualTourist/LocationDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LocationDetailViewController.swift
+//  virtualTourist
+//
+//  Created by Shir Bar Lev on 19/04/2022.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+    var dataManager: DataManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
