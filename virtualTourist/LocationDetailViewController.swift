98ef408be5eeabd17f98ea782750c38aefd8424b
Shir Bar Lev <shirblc@gmail.com>
2022-04-20 14:31:01 +0300

    Added handling for Photos in DetailViewController

--- virtualTourist/LocationDetailViewController.swift
+++ virtualTourist/LocationDetailViewController.swift
@@ -30,6 +30,8 @@ class LocationDetailViewController: UIViewController, UICollectionViewDelegate, 
         super.viewDidLoad()
         self.setupMap()
         self.setupAlbumFetchedResultsController()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -106,29 +108,62 @@ class LocationDetailViewController: UIViewController, UICollectionViewDelegate, 
     
     // MARK: UICollectionViewDataSource Methods
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.albumResultsController.fetchedObjects?.count ?? 0
+        // if we're veiewing albums, set the length of that array as number of items in view
+        if(currentMode == .PhotoAlbum) {
+            return self.albumResultsController.fetchedObjects?.count ?? 0
+        // otherwise make sure there's a photoResultsController; if there is, set the length of the fetched results as the number of items in view
+        } else {
+            guard let photoResultsController = photoResultsController else { return 0 }
+            
+            return photoResultsController.fetchedObjects?.count ?? 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let albumAtIndex = self.albumResultsController.object(at: indexPath)
-        
         let viewCell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! AlbumViewCell
-        viewCell.albumLabel.text = albumAtIndex.name
-        // randomly select an image from the set to display
-        let randomNumber = Int.random(in:0..<(albumAtIndex.photos?.count ?? 0))
-        let photosArray = albumAtIndex.photos?.allObjects as! [Photo]
-        if let selectedPhoto = photosArray[randomNumber].photo {
-            viewCell.albumImage.image = UIImage(data: selectedPhoto)
+        
+        // if we're in albums mode, show the album in the given position and its title
+        if(currentMode == .PhotoAlbum) {
+            let albumAtIndex = self.albumResultsController.object(at: indexPath)
+            
+            DispatchQueue.main.async {
+                viewCell.albumLabel.isHidden = false
+                viewCell.albumLabel.text = albumAtIndex.name
+                // randomly select an image from the set to display
+                let randomNumber = Int.random(in:0..<(albumAtIndex.photos?.count ?? 0))
+                let photosArray = albumAtIndex.photos?.allObjects as! [Photo]
+                if let selectedPhoto = photosArray[randomNumber].photo {
+                    viewCell.albumImage.image = UIImage(data: selectedPhoto)
+                    viewCell.albumImage.alpha = 0.5
+                }
+            }
+        // otherwise show the image in the given position
+        } else {
+            let photoAtIndex = self.photoResultsController?.object(at: indexPath)
+            
+            DispatchQueue.main.async {
+                viewCell.albumLabel.isHidden = true
+                viewCell.albumImage.alpha = 1
+                
+                if let photoAtIndex = photoAtIndex, let photo = photoAtIndex.photo {
+                    viewCell.albumImage.image = UIImage(data: photo)
+                }
+            }
         }
         
         return viewCell
     }
     
-    // MARK: UICollectionViewDataSource Methods
+    // MARK: UICollectionViewDelegate Methods
     func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
         return self.currentMode == .PhotoAlbum ? true : false
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedAlbum = self.albumResultsController.object(at: indexPath)
+        setupPhotoFetchedResultsController(selectedAlbum: selectedAlbum)
+    }
+        
     // MARK: Utilities
     // showErrorAlert
     // Displays an error alert that allows the user to optionally retry
